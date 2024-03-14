b608fb4b9a2cc586ec4a92c4186c7037c6a6d5fd
bnassri <82914301+bnassri@users.noreply.github.com>
2024-03-13 23:40:00 -0400

    Commented out alert and added function linked to html page

--- Basic_JavaScript_1.js
+++ Basic_JavaScript_1.js
@@ -1,13 +1,12 @@
 
-alert("Hello, World!  Here's an alert box!")
+//alert("Hello, World!  Here's an alert box!")
 
 
 
-<script>
-	function My_First_Function() {
-		var String = "Kiss me, I'm Irish!";
-		var result = String.fontcolor("green");
-		document.getElementById("Irish").innerHTML =
-		result;
-	}
-</script>
+
+function My_First_Function() {
+	var String = "Kiss me, I'm Irish!";
+	var result = String.fontcolor("green");
+	document.getElementById("Irish").innerHTML =
+	result;
+}
