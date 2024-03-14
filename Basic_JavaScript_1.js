3552f0e7e60916e313076e285c6623f99c7f7b02
bnassri <82914301+bnassri@users.noreply.github.com>
2024-03-13 23:18:36 -0400

    Added new function

--- Basic_JavaScript_1.js
+++ Basic_JavaScript_1.js
@@ -1,2 +1,13 @@
 
 alert("Hello, World!  Here's an alert box!")
+
+
+
+<script>
+	function My_First_Function() {
+		var String = "Kiss me, I'm Irish!";
+		var result = String.fontcolor("green");
+		document.getElementById("Irish").innerHTML =
+		result;
+	}
+</script>
